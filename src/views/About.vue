48286f070acf5dd8383f41948b01f1c08924fee4
rom38 <roman_imp@mail.ru>
2022-02-03 18:45:47 +0800

    1 file modified

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,35 +1,37 @@
 <template>
   <h1>About</h1>
-  <h2>{{count}}</h2>
-  <button class="btn btn-primary" @click='increment()' >add_temp</button>
+  <h2>{{ count }}</h2>
+  <button class="btn btn-primary" @click="increment()">add_temp</button>
   <p></p>
   <div class="input-group mb-3">
-  <input type="text" class="form-control">
-  <button class="btn btn-outline-secondary" @click='add_pat()'>add patient</button>
+    <input type="text" class="form-control" />
+    <button class="btn btn-outline-secondary" @click="add_pat()">add patient</button>
   </div>
-  <ul class="list-group list-group-numbered" >
-  <li class="list-group-item" v-for='pat in patients'>{{pat}}</li>
+  <ul class="list-group list-group-numbered">
+    <li class="list-group-item" v-for="pat in patients">{{ pat }}</li>
   </ul>
 </template>
 
 <script>
-import {store} from '../store'
+import { store } from '../store'
 //import { Store } from 'vuex'
 export default {
   name: 'HelloWorld',
   computed: {
-    count(){
-    return store.state.count},
-    patients(){
-    return store.state.patients}
+    count() {
+      return store.state.count
+    },
+    patients() {
+      return store.state.patients
+    },
   },
   methods: {
-    increment () {
+    increment() {
       return store.commit('increment')
     },
-    add_pat () {
+    add_pat() {
       return store.commit('add_pat')
-    }
-}
+    },
+  },
 }
 </script>
